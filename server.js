65b8b874e793414f9f2db4bb192d1e69d9908e83
Joyson Puvanasingham <60553801+enochj316@users.noreply.github.com>
2021-02-11 18:46:23 -0500

    updated

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.use(express.static("public"));
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workout',
     {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
     }
-);
+  );
 
 app.use(require("./routes/api.js"));
 app.use(require("./routes/html.js"));
