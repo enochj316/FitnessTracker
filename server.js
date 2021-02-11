af8a81e83c0337a179be6a3c52d494d66b168ea9
Joyson Puvanasingham <60553801+enochj316@users.noreply.github.com>
2021-02-11 17:29:09 -0500

    updated

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ app.use(express.json());
 //use static files
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittrack", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/FitnessTracker", {
+    useNewUrlParser: true
+});
 
 //require('./seeders/seed')
 
